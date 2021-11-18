4005cacbc56c08a4def4391d8d943f17c060486a
santi-herrera <sherrerasendes@udesa.edu.ar>
2021-11-18 20:32:01 -0300

    Detalle generos Santi Agus

--- js/genres.js
+++ js/genres.js
@@ -16,14 +16,53 @@ let urlGenresMovie= "https://api.themoviedb.org/3/genre/movie/list?api_key=531fb
 console.log(data.genres);
 let genresMovie= document.querySelector(".contenedorGenres")
 let genresLista="";
-
+let info = data.genres
 for (let i=0; i<19; i++){
     console.log(data.genres[i]);
-    genresLista+= `<article class="articulosGenres"> <a href="detail-genres.html"> <h2> ${data.genres[i].name} </h2> </a></article>`;
+    genresLista+= ` <article class="articulosGenres"> <a href="detail-genres.html?id=${info[i].id}&nombre=${data.genres[i].name}"> <h2> ${data.genres[i].name} </h2> </a></article>`;
     
     
    }
    genresMovie.innerHTML += genresLista
+   
+})
+
+.catch(function (error) {
+    console.log(error)
+}
+)
+
+
+
+
+
+
+//series
+
+
+let urlGenresSeries= "https://api.themoviedb.org/3/genre/tv/list?api_key=531fb37b1ce0765b98bdef56966d3e48"  
+
+
+
+
+    fetch(urlGenresSeries)
+    .then(function(response){
+      return response.json()
+
+    })
+
+   .then(function(data){
+console.log(data.genres);
+let genresSeries= document.querySelector(".contenedorGenresSeries")
+let genresListaSeries="";
+
+for (let i=0; i<16; i++){
+    console.log(data.genres[i]);
+    genresListaSeries+= ` <article class="articulosGenres"> <a href="detail-genres.html"> <h2> ${data.genres[i].name} </h2> </a></article>`;
+    
+    
+   }
+   genresSeries.innerHTML += genresListaSeries
    
 })
 
